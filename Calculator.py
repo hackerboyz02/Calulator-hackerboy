938e1587d296eb9047202f8a29268a2d63847f45
hackerboyz02 <150543390+hackerboyz02@users.noreply.github.com>
2024-01-24 11:28:40 +0530

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,24 @@
+print(''' 
+__________________________________
+| simple = calculator             |
+| USES To Add +,Subtract -,       |
+| Multiply * , Divide /           |
+| REGARDS:: Hackerboy             | 
+| CHANEL:: https://t.me/h4cker_boy|
+|_________________________________|
 
+ we Make first termux tool give feedback on our telegram channel''')
+
+num1  = eval(input("enter the number"))
+num2= eval(input("enter the number"))
+opr = input ("enter the oprator")
+if opr == '+' :
+  print(num1+num2)
+if opr == '-' :
+  print(num1-num2)
+if opr == '*' :
+  print(num1*num2)
+elif opr == '/' :
+  print (num1/num2)
+else :
+  print ("invailed operator")
